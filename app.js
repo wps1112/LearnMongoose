fa45b67c83d2fdefcdc3fa5ea1649eeb007e78da
wps1112 <569456351@qq.com>
2015-11-24 10:01:45 +0800

    更改登陆界面，为用户登录加盐散列。

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//加载路由模块
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+//指定视图路径以及渲染引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -20,6 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//指定静态资源加载路径
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- public/js/lbs.js
+++ public/js/lbs.js
@@ -6,9 +6,22 @@ function loadJScript() {
 }
 
 function init() {
-    var map = new BMap.Map("allmap");            // 创建Map实例
-    var point = new BMap.Point(116.404, 39.915); // 创建点坐标
+    var map = new BMap.Map("allmap");
+    var point = new BMap.Point(116.404, 39.915);
     map.centerAndZoom(point, 15);
-    map.enableScrollWheelZoom();                 //启用滚轮放大缩小
+    map.enableScrollWheelZoom();
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function (r) {
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+            alert('浣犵殑浣嶇疆' + r.point.lng + ',' + r.point.lat);
+        }
+        else {
+            alert('failed' + this.getStatus());
+        }
+    }, {enableHighAccuracy: true})
 }
-window.onload = loadJScript;  //异步加载地图
+window.onload = loadJScript;
